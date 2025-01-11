37f327d92c5e40c319ba7feeaeddd9e04aee6df3
Game-Developer123 <92021716+Game-Developer123@users.noreply.github.com>
2025-01-11 14:36:46 +0530

    Create Vital.java

---
+++ Vital.java
@@ -0,0 +1,11 @@
+public class Vital {
+    private int id;
+    private int patientId;
+    private double bpHigh;
+    private double bpLow;
+    private double saturation;
+    private double spo2;
+    private Timestamp recordedAt;
+
+    // Getters and setters
+}

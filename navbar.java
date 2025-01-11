16159c070aff400bc0b17a9c95b777b055d23a29
Game-Developer123 <92021716+Game-Developer123@users.noreply.github.com>
2025-01-11 14:49:44 +0530

    Create navbar.java

---
+++ navbar.java
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Health Logger</title>
+</head>
+<body>
+    <nav>
+        <a href="patient_list">Patients</a>
+        <a href="threshold_alerts">Alerts</a>
+        <a href="logout">Logout</a>
+    </nav>
+</body>
+</html>

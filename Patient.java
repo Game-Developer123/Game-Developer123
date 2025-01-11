6a995211d5f8684938649e995ba829b82b922de9
Game-Developer123 <92021716+Game-Developer123@users.noreply.github.com>
2025-01-11 14:36:30 +0530

    Create Patient.java

---
+++ Patient.java
@@ -0,0 +1,10 @@
+public class Patient {
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private String diagnosis;
+    private String remarks;
+
+    // Getters and setters
+}
